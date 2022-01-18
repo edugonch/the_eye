6249ebd7254a17f17ec841f27cf89f382dc1e843
Eduardo Gonzalez <edugonch@gmail.com>
2022-01-18 16:15:58 -0600

    Add first migrations

--- api/models.py
+++ api/models.py
@@ -1,3 +1,13 @@
 from django.db import models
 
-# Create your models here.
+class Session(models.Model):
+    id = models.CharField(primary_key=True, max_length=250, unique=True, default=id)
+    created_at = models.DateField(auto_now_add=True)
+    updated_at = models.DateField(auto_now=True)
+
+class Event(models.Model):
+    session = models.ForeignKey(Session, on_delete=models.CASCADE)
+    category = models.CharField(max_length=250)
+    name = models.CharField(max_length=250)
+    data = models.JSONField("data", default={})
+    time_of_occurrence = models.DateField(auto_now_add=True)
--- the_eye/settings.py
+++ the_eye/settings.py
@@ -37,6 +37,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'api',
+    'the_eye'
 ]
 
 MIDDLEWARE = [
